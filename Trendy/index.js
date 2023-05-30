22179c02442d29a8a9f048d891177c1a90f41aa1
jcmr5720 <camilomr57@gmail.com>
2023-05-30 11:43:12 -0500

    Commit 8

---
+++ Trendy/index.js
@@ -0,0 +1,21 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.add("show");
+  }
+  
+  function hideMenu() {
+    document.getElementById("myDropdown").classList.remove("show");
+  }
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+  
