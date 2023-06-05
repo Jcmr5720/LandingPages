770e6a9fb11196fe0b6f88110113f25736fa84db
jcmr5720 <camilomr57@gmail.com>
2023-06-05 17:42:50 -0500

    commit

--- Proyecto01/viteproject01/vite.config.js
+++ Proyecto01/viteproject01/vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import React from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [React()]
+    plugins: [React()],
+    base: 'CSSRESPON1/Proyecto01/viteproject01/'
 })
