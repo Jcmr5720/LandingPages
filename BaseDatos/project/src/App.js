fc54fb737587a1c6e2961f6f73bb987f6cfe0d68
jcmr5720 <camilomr57@gmail.com>
2023-06-07 11:22:42 -0500

    commit

--- BaseDatos/project/src/App.js
+++ BaseDatos/project/src/App.js
@@ -1,62 +1,69 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import logo from './imgs/logo.png';
+import { AiOutlineBook, AiOutlineArrowDown, AiOutlineWhatsApp, AiOutlineShareAlt, AiOutlineClose } from "react-icons/ai";
 
 const App = () => {
-  const [form, setForm] = useState({
-    nombre: '',
-    edad: ''
-  });
+  const [cuadro, setCuadro] = useState(false);
 
-  const span1 = useRef('');
-  const input1 = useRef();
-  const input2 = useRef();
-
-  const cambio = (e) => {
-    setForm({ ...form, [e.target.name]: e.target.value });
+  const bCatalogo = () => {
+    window.location.href = "#";
   };
 
-  const borrar = (e) => {
-    e.preventDefault();
-    input1.current.value = '';
-    input2.current.value = '';
+  const bWhatsapp = () => {
+    window.location.href = "#";
   };
 
-  const enviar = (e) => {
-    e.preventDefault();
-    if (form.nombre === '' || form.edad === '') {
-      span1.current.innerHTML = `Error: no se han completado todos los campos`;
-    } else if (form.edad < 0) {
-      span1.current.innerHTML = `Error: la edad no puede ser negativa`;
-    } else {
-      span1.current.innerHTML = `Hola, tus datos son:<br/>Nombre: ${form.nombre}<br/>Edad: ${form.edad}`;
-      console.log(form);
-    }
+  const bShare = () => {
+    setCuadro(true);
+  };
+
+  const closeCuadro = () => {
+    setCuadro(false);
   };
 
   return (
     <>
-      <section className='main1'>
-        <div className='divformulario'>
-          <h1>Formulario con React</h1>
-          <form className='formulario' onSubmit={enviar}>
-            <label>
-              Nombre
-              <input type='text' name='nombre' value={form.nombre} placeholder='Escribe tu nombre' ref={input1} onChange={cambio} />
-            </label>
-            <label>
-              Edad
-              <input type='number' name='edad' value={form.edad} placeholder='Escribe tu edad' ref={input2} onChange={cambio} />
-            </label>
-            <div className='formButtons'>
-            <button type='submit'>Enviar</button>
-            <button type='button' onClick={borrar}>Borrar datos</button>
+      <header>
+        <div className='headerLogo'>
+          <span className='btShare' onClick={bShare}><AiOutlineShareAlt size={32} /></span>
+        </div>
+      </header>
+      <main>
+        <div className='logoi'>
+          <img src={logo} alt='Logo' onClick={bShare} />
+        </div>
+        <div className='text'>
+          <span className='text1'>LUCY'S SHOP✨</span>
+          <span className='text2'>Tienda virtual de maquillaje, cuidado facial y accesorios</span>
+        </div>
+        <div className='buttons'>
+          <div className='button1' onClick={bCatalogo}>
+            <span className='logo'><AiOutlineBook size={32} /></span>
+            <span className='b1'>CATÁLOGO</span>
+            <span className='down'><AiOutlineArrowDown size={16} /></span>
+          </div>
+          <div className='button2' onClick={bWhatsapp}>
+            <span className='logo'><AiOutlineWhatsApp size={32} /></span>
+            <span className='b2'>WHATSAPP</span>
+            <span className='down'><AiOutlineArrowDown size={16} /></span>
+          </div>
+        </div>
+      </main>
+      {cuadro && (
+        <div className='cuadro'>
+          <div className='cuadro1'>
+            <div className='buttons'>
+              <span></span>
+              <span>Compartir</span>
+              <span><AiOutlineClose size={16} onClick={closeCuadro} /></span>
             </div>
-          </form>
+            <div className='windows'>
+              <span>Hola mundo jeje</span>
+            </div>
+          </div>
         </div>
-        <div className='divtext'>
-          <span ref={span1}></span>
-        </div>
-      </section>
+      )}
     </>
   );
 };
